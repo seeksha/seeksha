341e9b571d8c82b7025431a162702c861e83af47
lbh <seeksha4@gmail.com>
2018-11-22 00:01:00 +0900

    UI Prototype03 -change: My Reservation-

---
+++ seeksha/ReservationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReservationViewController.swift
+//  seeksha
+//
+//  Created by lbh on 21/11/2018.
+//  Copyright © 2018 lbh. All rights reserved.
+//
+
+import UIKit
+
+class ReservationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
